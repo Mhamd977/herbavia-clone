46360ecd5ce0da4660f68f71845b2b8f2aee82ab
Mhamd977 <mohammad.you.977@gmail.com>
2023-09-25 16:06:06 +0300

    project

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  const eHFn = () => {
+    $(".eH-Area").each(function () {
+      $(this).find(".eH").matchHeight();
+      $(this).find(".eH2").matchHeight();
+    });
+  };
+  eHFn();
+  $(document).on("click scroll resize", function () {
+    eHFn();
+  });
+});
