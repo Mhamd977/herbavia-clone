ec22cb395b890aea9c948048b4a33d2a793f1383
Mhamd977 <mohammad.you.977@gmail.com>
2023-09-26 10:27:16 +0300

    about us

--- js/main.js
+++ js/main.js
@@ -913,7 +913,7 @@
             dots: false,
             infinite: true,
             speed: 300,
-            slidesToShow: 3,
+            slidesToShow: 5,
             slidesToScroll: 1,
             prevArrow: '<a class="slick-prev"><i class="fas fa-arrow-left" alt="Arrow Icon"></i></a>',
             nextArrow: '<a class="slick-next"><i class="fas fa-arrow-right" alt="Arrow Icon"></i></a>',
