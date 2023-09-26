255c753f797d3e2ac96ce1523501584c66531fdc
jamilshehab <shehabjamil20@gmail.com>
2023-09-26 15:02:24 +0300

    Blog Details and Blog Added

--- js/script.js
+++ js/script.js
@@ -11,3 +11,17 @@ $(document).ready(function () {
   });
 });
  
+$('.ltn__blog-img').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',
+  removalDelay: 300,
+  gallery: {
+    enabled: true,
+    navigateByImgClick: true,
+    preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+  },
+  zoom: {
+    enabled: true,
+    duration: 300 // don't foget to change the duration also in CSS
+  }
+});
